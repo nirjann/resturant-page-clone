eac2eddad9f0819a9ef3d899d4ba30f8190f54e1
nirjann <nirjann3@gmail.com>
2021-06-10 23:48:54 +0530

    nearly done with the header

---
+++ src/app/header.js
@@ -0,0 +1,71 @@
+import '../style/header.scss';
+
+function createHeaderSkeleton() {
+    const headerSection = document.getElementById('header');
+    headerSection.classList.add('header');
+    const containerDiv = document.createElement('div');
+    containerDiv.classList.add('container', 'header-container');
+    headerSection.appendChild(containerDiv);
+
+    const navbar = document.createElement('div');
+    navbar.classList.add('navbar');
+    containerDiv.appendChild(navbar);
+
+   
+    const headerShowWrapper = document.createElement('div');
+    headerShowWrapper.classList.add('header-show');
+    containerDiv.appendChild(headerShowWrapper);
+    
+};
+
+function createNavbarSkeleton() {
+    const navbar = document.querySelector('.navbar');
+    const navLogo = document.createElement('div');
+    navLogo.classList.add('nav-logo');
+    const navMenuWrapper = document.createElement('div');
+    navMenuWrapper.classList.add('nav-menu-wrapper');
+    const navMenu = document.createElement('ul');
+    navMenu.classList.add('nav-menu')
+    for (let i = 0; i < 3; i++) {
+        let li = document.createElement('li');
+        li.classList.add('nav-item');
+        navMenu.appendChild(li);
+    }
+    navMenuWrapper.appendChild(navMenu);
+    navbar.appendChild(navLogo);
+    navbar.appendChild(navMenuWrapper);
+    
+}
+
+function renderNavbar() {
+    createHeaderSkeleton();
+    createNavbarSkeleton();
+
+    const navLogo = document.querySelector('.nav-logo');
+    navLogo.innerHTML = "<h2>EsoKhai</h2>";
+
+    const navItems = document.querySelectorAll('.nav-item');
+    let navLinkTexts = ['Home', 'Chef', 'Contact'];
+    let i = 0;
+    navItems.forEach(item => {
+        let a = document.createElement('a');
+        a.setAttribute('href', '#');
+        a.classList.add('nav-link', 'btn-secondary');
+        a.textContent = navLinkTexts[i];
+        i++;
+        item.appendChild(a);
+
+    });
+
+
+};
+
+function renderHeader() {
+    renderNavbar();
+    const headerShowWrapper = document.querySelector('.header-show');
+    headerShowWrapper.innerHTML = `
+        <h2 class = 'show-text'> PLEASE HAVE SOME FOOD </h2>
+        <a href = '#' class = 'btn-primary'>RESERVE TABLE </a>
+    `;
+}
+export {renderHeader};
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,5 @@
 import '../style/globals.scss';
+import '../style/helpers.scss';
+import {renderHeader} from './header';
+
+renderHeader();
