6e45f61226cf2ee0fc367f2cddba5dfa0eab4af6
nirjann <nirjann3@gmail.com>
2021-06-10 22:34:32 +0530

    skeleton nearly ready

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: {
+        index: "./src/app/index.js",
+        header: "./src/app/header.js",
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+
+    plugins: [
+        new htmlWebpackPlugin({
+            inject: 'body',
+            template: "./src/index.html",
+            
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+
+            {
+                test: /\.(jpg|jpeg|png|svg)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
